3ca8fe207dc0a17208020dffcc244114c613edbe
Tres Henry <tres@treshenry.net>
2014-05-10 15:03:59 -0700

    Added !img token to make it easy to format images with styles

--- views/helpers/markdown.js
+++ views/helpers/markdown.js
@@ -1,9 +1,18 @@
 var Marked = require('marked');
 
+var imgRegex = /^\!img\ (.*)$/gm;
+
 // HBS helper for markdown
 module.exports = function(hbs) {
   hbs.registerHelper('markdown', function(text, options) {
     if(typeof(text) === 'string') {
+
+      // Pre-processor for !img token (makes adding images with correct formatting easier)
+      text = text.replace(imgRegex, function(m, src) {
+        return '<span class="postaimg"><img src="' + src + '"></span>';
+      });
+
+      // Now parse as md
       return new hbs.SafeString(Marked(text));
     }
   });
